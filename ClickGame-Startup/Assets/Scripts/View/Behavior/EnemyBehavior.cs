f4ec2b9afd6fbaae264c90ce0ae454469f7487c1
4419-3-8\user <arrowleze18@gmail.com>
2017-12-21 21:12:46 +0800

    2017/12/21上課進度

---
+++ ClickGame-Startup/Assets/Scripts/View/Behavior/EnemyBehavior.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(MeshFader))]
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(HealthComponent))]
+
+public class EnemyBehavior : MonoBehaviour {
+
+    private MeshFader meshFader;
+    private Animator animator;
+    private AudioSource audioSource;
+    private HealthComponent healthComponent;
+
+    [SerializeField]
+    private AudioClip hurtClip;
+    [SerializeField]
+    private AudioClip deadClip;
+
+    public bool IsDead
+    {
+        get
+        {
+            return healthComponent.IsOver;
+        }
+    }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        meshFader = GetComponent<MeshFader>();
+        audioSource = GetComponent<AudioSource>();
+        healthComponent = GetComponent<HealthComponent>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(meshFader.FadeIn());
+    }
+    [ContextMenu("Test Execute")]
+    public void TestExecute()
+    {
+        StartCoroutine(Execute()) ;
+    }
+
+    #region Public Method
+    /// <summary>
+    /// 請呼叫我 開始敵人生命邏輯
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator Execute()
+    {
+        healthComponent.Init(100);
+        while (IsDead == false)
+        {
+            yield return null;
+        }
+        animator.SetTrigger("die");
+        audioSource.clip = deadClip;
+        audioSource.Play();
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        yield return StartCoroutine(meshFader.FadeOut());
+    }
+    #endregion
+
+    private void Update()
+    {
+        if (IsDead) return;
+        if (Input.GetButtonDown("Fire1")) DoDamage(50);
+    }
+
+    public void DoDamage(int attack)
+    {
+        healthComponent.Hurt(attack);
+        animator.SetTrigger("hurt");
+        audioSource.clip = hurtClip;
+        audioSource.Play();
+
+    }
+}
---
+++ ClickGame-Startup/Assets/Scripts/View/Component/HealthComponent.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthComponent : MonoBehaviour {
+
+    public Slider healthSlider;
+    private int currentHealth;
+    public float smooth = 5f;
+
+    public bool IsOver  //告知血量為零
+    {
+        get
+        {
+            return currentHealth <= healthSlider.minValue;
+        }
+    }
+
+    [ContextMenu("Test Hurt")]
+    private void TestHurt()
+    {
+        Init(100);
+        Hurt(50);
+    }
+    public void Init(int maxHealth)
+    {
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = healthSlider.maxValue;
+        currentHealth = (int)healthSlider.maxValue;
+    }
+
+    public void Hurt(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = (int)Mathf.Max(healthSlider.minValue, currentHealth) ;
+    }
+
+	void Update ()
+    {
+        healthSlider.value = Mathf.Lerp(healthSlider.value,currentHealth,Time.deltaTime * smooth) ;	
+	}
+}
